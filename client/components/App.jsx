3f6854e1d5fd0897ad4a7cc8b3220bb8adc2f031
Jeremy C <102775355+JeremyC3@users.noreply.github.com>
2023-05-27 09:43:48 -0700

    Added App.jsx framework for Frontend to split

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,17 @@ import React, { Component, useState } from 'react';
 
 function App() {
   //State for Key-Value Pairs
-
+  const [kvpArr, setKvp] = useState([{ name: '', value: 'String' }]);
   //State for Past Projects
+
+  //functions to drill down
 
   return (
     <div>
-      <h1>Hello world! it me</h1>
+      <h1>Schema Dreama</h1>
+      <div>InputButton</div>
+      <div>PastProjects</div>
+      <div>SchemaMaker</div>
     </div>
   );
 }
