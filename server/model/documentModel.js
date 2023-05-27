25b04553c6a0607211c90fced52d70b63d722a48
Stephen Lemlek <stephenlemlek@Stephens-MBP.lan>
2023-05-27 06:34:41 -0500

    day after start  commit

--- server/model/documentModel.js
+++ server/model/documentModel.js
@@ -9,4 +9,10 @@ const Schema = mongoose.Schema;
 //create model for the schema collection above
 
 
-//export schema
+//export schema this is khaile oh it's chill i'll just practice mongo on my own outside please feeel free to shut down and have a good night! sounds good~ thanks again steve!
+
+//you guys in here? yes but i'm going to log off this is khaile
+
+//kk have a good one!
+//jimmy, i'll just leave my labtop open tilll i come back so  you can use it
+//if you have any question slack me!  you too, have a good one.  i'll just leave this up since it's no big deal anyway//see you tomorrow!
