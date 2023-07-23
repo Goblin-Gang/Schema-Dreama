e45067f7c8c320c5df00035d84c84ad70e2d8af0
Steve Lemlek <stevelemlek@gmail.com>
2023-07-23 11:55:35 -0500

    feat: commented userController and removed console.logs

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,60 +1,93 @@
-const mongoose = require('mongoose')
-const User = require('../model/userModel')
-//change this to User when User Model is completed
-//select all User methods and change to User
+const mongoose = require('mongoose');
+const User = require('../model/userModel');
 
-const userController = {}
+//middleware to handle user documents in the db
+
+const userController = {};
+
+//POST requst middleware handler
 
 userController.verifyLogin = async (req, res, next) => {
-    console.log(req.body, 'in the verifyUserName')
-    const { username, password } = req.body
-    try {
-        const result = await User.findOne({ username: username })
-        if (!result) {
-            res.redirect('/signup')
-        } else {
-            const verified = await User.findOne({ username, password })
-            if (verified) {
-                res.locals.verified = true
-                res.locals.userId = verified._id
-                return next()
-            } else {
-                res.locals.verified = false
-                return next()
-            }
-        }
-    }
-    catch (error) {
-        return next({
-            log: 'error in the verifyLogin middleware',
-            message: error
-        })
-    }
-}
+	//deconstruct username & password from req.body
 
+	const { username, password } = req.body;
+
+	try {
+		//query db to find username
+
+		const result = await User.findOne({ username: username });
+
+		//redirect if no username found
+		if (!result) {
+
+			res.redirect('/signup');
+
+		} else {
+
+			//query db to verify password, if username exists
+
+			const verified = await User.findOne({ username, password });
+
+			//if verified, add verification details to res.locals
+
+			if (verified) {
+				res.locals.verified = true;
+				res.locals.userId = verified._id;
+				return next();
+
+			//if unverified, return false as a property on res.locals
+			} else {
+				res.locals.verified = false;
+				return next();
+			}
+		}
+	
+	//error catch
+		
+	} catch (error) {
+		return next({
+			log: 'error in the verifyLogin middleware',
+			message: error,
+		});
+	}
+};
+
+//middleware to handle signups
 
 userController.signUp = async (req, res, next) => {
-    const { username, password } = req. body;
-    
-    console.log('signUp', username, password)
-    
-    if(username === undefined || password === undefined) {
-        res.redirect('/signup');}
-    else {
-        try {
 
-            console.log('sign up before call')
-            const newUser = await User.create({username, password});
-            res.locals.user = newUser;
-            console.log('signup in call', res.locals.user.username)
-            next()         
-        } catch (error) {
-            next({
-                log: 'error in the signUp middleware controller',
-                err: error
-            })         
-        }
-    }
-}
+	//deconstruct username and password from request body
 
-module.exports = userController
+	const { username, password } = req.body;
+
+	//if either no username or undefined, redirec to signup page
+
+	if (username === undefined || password === undefined) {
+
+		res.redirect('/signup');
+
+	} else {
+		try {
+
+			//query db with username and password to create new user document
+
+			const newUser = await User.create({ username, password });
+
+			//add new user output to res.locals
+
+			res.locals.user = newUser;
+
+			next();
+
+			
+			//error catch
+		} catch (error) {
+			next({
+				log: 'error in the signUp middleware controller',
+				err: error,
+			});
+		}
+	}
+};
+
+module.exports = userController;
